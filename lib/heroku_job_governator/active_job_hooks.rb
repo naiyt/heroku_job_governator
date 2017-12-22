8624380a93dfbdb84220a9bffa5d9276b8aafa14
Brandon Broschinsky <brandonbr@instructure.com>
2017-12-22 09:02:49 -0700

    active_job errors

--- lib/heroku_job_governator/active_job_hooks.rb
+++ lib/heroku_job_governator/active_job_hooks.rb
@@ -1,13 +1,5 @@
 module GovernedJob 
   module ActiveJobHooks
-    after_perform do |job|
-      scale_down(queue(args))
-    end
-
-    
-    after_enqueue do |job|
-      scale_up(queue(args))
-    end
     
     def queue(*args)
       "TODO"
